8d9bdba06ece3170601aa9e6f3230ced82fd3b20
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2025-02-13 17:46:17 +0300

    fix bags

--- NSWorkingWithSpecificRowsColumns/Program.cs
+++ NSWorkingWithSpecificRowsColumns/Program.cs
@@ -9,35 +9,35 @@ namespace NSWorkingWithSpecificRowsColumns
             Random random = new Random();
             int randomMin = 1;
             int randomMax = 10;
-            int[,] array = new int[4, 4];
+            int[,] numbers = new int[4, 4];
             int sum = 0;
             int multiplication = 1;
-            int line = 2;
-            int column = 1;
+            int lineNumber = 2;
+            int columnNumber = 1;
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    array[i, j] = random.Next(randomMin, randomMax);
-                    Console.Write(array[i, j] + " ");
+                    numbers[i, j] = random.Next(randomMin, randomMax);
+                    Console.Write(numbers[i, j] + " ");
                 }
 
                 Console.WriteLine();
             }
 
-            for (int i = 0; i < array.GetLength(1); i++)
+            for (int i = 0; i < numbers.GetLength(1); i++)
             {
-                sum += array[line - 1, i];
+                sum += numbers[lineNumber - 1, i];
             }
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                multiplication *= array[i, column - 1];
+                multiplication *= numbers[i, columnNumber - 1];
             }
 
-            Console.WriteLine($"Сумма второй строки: {sum}");
-            Console.WriteLine($"Произведение первого столбца: {multiplication}");
+            Console.WriteLine($"Сумма {lineNumber} строки: {sum}");
+            Console.WriteLine($"Произведение {columnNumber} столбца: {multiplication}");
         }
     }
 }
